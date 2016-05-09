5c4fe677df1a296410860a1bdf11fdea40dd8be6
WingYee Mak <wwymak@gmail.com>
2016-05-09 09:18:31 +0100

    experiments WIP

--- imageAnalyser.js
+++ imageAnalyser.js
@@ -17,16 +17,89 @@ var flickrOptions = {
   secret: config.flickr.secretKey
 };
 
+// var flickrPandasSearchOpts = {
+//   panda_name: 'wang wang',
+//   per_page:500
+// };
+//
+// Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+//   if(error) {
+//     console.log(error)
+//   }
+//   console.log('flickr pandas');
+//   // flickr.panda.getList((err, result) => {
+//   //   console.log(err, result.pandas.panda[0], result.pandas.panda[1], result.pandas.panda[2])
+//   // })
+//   flickr.panda.getPhotos(flickrPandasSearchOpts, (err, result) => {
+//     if (err != null) {
+//       console.log(err);
+//       return;
+//     } else {
+//       if (!result || !result.photos || !result.photos.photo.length == 0) {
+//         console.log('no results')
+//         return;
+//       }
+//     }
+//     console.log(result, result.photos.photo[0], result.photos.photo.length);
+//     let imagesRawData = result.photos.photo;
+//     let imageUrls = imagesRawData.map(photo => {
+//       return {
+//         imgID: photo.id,
+//         imgUrl: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+//         imgTitle: photo.title
+//
+//       };
+//
+//     });
+//     //use map limit both to respect api and also colorthief gets confused and images
+//     //don't write so nicely and likely to err out
+//     async.mapLimit(imageUrls, 5, (urlObj, callback) => {
+//       request.get(
+//           {
+//             url: urlObj.imgUrl,
+//             encoding: 'binary'
+//           }, (err, resp, body) => {
+//             var filename = urlObj.imgID + '.jpg';
+//             var path = 'tmp/' + filename;
+//             fs.writeFile(path, body, 'binary', function () {
+//               var palette = colorThief.getPalette(path, 4);
+//               callback(err, palette);
+//
+//               rimraf(path, [], function (err) {
+//                 if (err) {
+//                   console.log(err)
+//                 }
+//               });
+//             });
+//           });
+//     }, (err, result) => {
+//       let allColors = result[0];
+//       for (let i = 1; i< result.length; i++){
+//         allColors = [...allColors, ...result[i]];
+//       }
+//       console.log(allColors[0] , 'done')
+//       fs.writeFile('colorPandas.json', JSON.stringify(allColors), (err, cb) => {
+//         if (err !== null) {
+//           console.log(err);
+//         } else {
+//           console.log('file saved');
+//         }
+//       })
+//     });
+//
+//   })
+// });
+
 Flickr.tokenOnly(flickrOptions, function(error, flickr) {
   if(error) {
     console.log(error)
   }
   console.log('flickr');
   flickr.photos.search({
-    text: 'happy',
-    per_page: 100
+    text: 'green',
+    per_page: 500
   }, (err, result) => {
-    
+
     console.log(result.photos.photo[0], result.photos.photo.length);
     let imagesRawData = result.photos.photo;
     let imageUrls = imagesRawData.map(photo => {
@@ -34,9 +107,9 @@ Flickr.tokenOnly(flickrOptions, function(error, flickr) {
         imgID: photo.id,
         imgUrl: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
         imgTitle: photo.title
-        
+
       };
-          
+
     });
     //use map limit both to respect api and also colorthief gets confused and images
     //don't write so nicely and likely to err out
@@ -74,18 +147,6 @@ Flickr.tokenOnly(flickrOptions, function(error, flickr) {
       })
     });
 
-    // console.log(imageUrls[0]);
-    
-    let test = imageUrls.slice(0,5);
-
-    
-    var testPromise =  [];
-    // test.forEach((d, i) => {
-    //   testPromise.push(setTimeout(() => {getPalette(d.imgUrl, d.imgID, d); console.log(d);}, 500 * i));
-    // });
-    
-    
-
   })
 });
 
@@ -108,28 +169,7 @@ function getPalette(imageUrl, id, dataObj) {
 
         });
       });
-  // request.get({url: imageUrl,
-  //   encoding: 'binary'}, (err, resp, body) => {
-  //   if (!err && resp.statusCode == 200) {
-  //     var filename = id + '.jpg';
-  //     var path = 'tmp/' + filename;
-  //     fs.writeFile(path, body, 'binary', function () {
-  //       var palette = colorThief.getPalette(path, 4);
-  //       // console.log(palette);
-  //       dataObj.colorPalette = palette;
-  //      
-  //         rimraf(path, [], function(err) {
-  //           if(err) {
-  //             console.log(err)
-  //           }
-  //         });
-  //      
-  //       console.log(dataObj)
-  //
-  //     });
-  //
-  //
-  //   }
-  // });
+
 
 }
+
